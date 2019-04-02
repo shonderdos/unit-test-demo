ac6c9525a39a086a52520dadbbec365075f8794c
Serge Honderdos <shonderdos@icloud.com>
2019-04-02 10:29:42 +0200

    set up boilerplate code to run tests

--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -1,9 +1,19 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { CounterComponent } from './counter.component';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {CounterComponent} from './counter.component';
 
 describe('CounterComponent', () => {
   let component: CounterComponent;
   let fixture: ComponentFixture<CounterComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [CounterComponent]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CounterComponent);
+    component = fixture.nativeElement;
+  });
 
 });
