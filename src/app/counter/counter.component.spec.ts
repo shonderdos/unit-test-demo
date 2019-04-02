ca25ec2d635225567db5872ac998b837bcf80120
Serge Honderdos <shonderdos@icloud.com>
2019-04-02 11:18:34 +0200

    test default value of paragraph tag

--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -1,9 +1,13 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {CounterComponent} from './counter.component';
+import {DebugElement} from '@angular/core';
+import {By} from '@angular/platform-browser';
 
 describe('CounterComponent', () => {
   let component: CounterComponent;
   let fixture: ComponentFixture<CounterComponent>;
+  let element: DebugElement;
+  let htmlElement: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -14,6 +18,14 @@ describe('CounterComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CounterComponent);
     component = fixture.nativeElement;
+    element = fixture.debugElement.query(By.css('p'));
+    htmlElement = element.nativeElement;
+  });
+
+  it('should display correct number initially', () => {
+    const startValue = htmlElement.textContent;
+
+    expect(startValue).toBe('Number: 1');
   });
 
 });
