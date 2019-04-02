99dcfadf3dad7f2cc62a71e028ba5023153319b2
Serge Honderdos <shonderdos@icloud.com>
2019-04-02 12:22:55 +0200

    add template test for counter

---
+++ src/app/counter/counter.component.spec.ts
@@ -0,0 +1,9 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CounterComponent } from './counter.component';
+
+describe('CounterComponent', () => {
+  let component: CounterComponent;
+  let fixture: ComponentFixture<CounterComponent>;
+
+});
